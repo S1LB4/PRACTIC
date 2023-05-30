c0fba46d5f88d8cd48da470ccb26e816b2930ff7
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-05-30 11:23:15 -0500

    Add files via upload

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export default{
+  apiKey: "AIzaSyAoBg8-9RK8Av9SUeZQbjwn79qU1y3A-tA",
+  authDomain: "practica-3-e9af6.firebaseapp.com",
+  projectId: "practica-3-e9af6",
+  storageBucket: "practica-3-e9af6.appspot.com",
+  messagingSenderId: "179259661211",
+  appId: "1:179259661211:web:c120ac88fd9a05f8ba1f3e"
+};
---
+++ src/utils/firebase.ts
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "../firebaseConfig";
+import { Product } from "../types/products";
+import { collection, addDoc, getFirestore, getDocs } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const saveProductInDB = async (product: Product) => {
+
+try {
+    await addDoc(collection(db, "products"), product);
+    return true;
+} catch (e) {
+  console.error("Error adding document: ", e);
+  return false;
+}
+};
+
+const getProductsFromDB = async(): Promise<Product[]> => {
+    const resp: Product[] = [];
+    const querySnapshot = await getDocs(collection(db, "products"));
+    querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+  resp.push({...doc.data(),} as Product);
+});
+return resp;
+}
+
+export default { saveProductInDB, getProductsFromDB };
